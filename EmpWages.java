01d5355b571af4cc4c343016dd8c0dbcddf649d2
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-10-26 06:20:21 +0530

    added: calculate wages monthly

--- EmpWages.java
+++ EmpWages.java
@@ -1,46 +1,42 @@
 package org.emp;
 
 import java.util.Random;
-import java.util.Scanner;
 
-//partTime Employee Wages Using Switch_Case
-
-public class EmpPartTimeScase {
+public class EmpWagesMonthly {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		//Calculate Employee Wages Monthly.
 		
-		Scanner in = new Scanner(System.in);
 		
 		int perHour = 20;
 		int FullDayHour = 8;
-		int day;
-		
+		int WorkingDayMonth = 20;
+
+		System.out.println("-------Employee_Wages--------");
+
 		Random rn=new Random();
 		int rand=rn.nextInt(3);
-		
-		switch(rand) {
-		
-		case 1: 
-			System.out.println("-------FullTime_Employee_Wages-------");
-			System.out.println("Enter no Days Employee Worked : ");
-			day = in.nextInt();
-			int FullTimeWages = perHour * (FullDayHour * day);
-			System.out.println("Full_Time_Wages_for " + day + " days : S" + FullTimeWages + " Rs.");
+
+		switch (rand) {
+
+		case 1:
+			System.out.println("-------FullTime_Employee_Wages_Monthly-------");
+			int FullTimeWages = perHour * (FullDayHour * WorkingDayMonth);
+			System.out.println("Full_Time_Wages_for " + WorkingDayMonth + " days : " + FullTimeWages + " Rs.");
 			break;
 		case 2:
-			System.out.println("-------PartTime_Employee_Wages-------");
-			System.out.println("Enter no Days Employee Worked : ");
-			day = in.nextInt();
-			int partTimeWages = (day * (FullDayHour / 2)) * perHour;
-			System.out.println("PartTime_Wages_for " + day + " days : " + partTimeWages + " Rs.");
-		break;
+			System.out.println("-------PartTime_Employee_Wages_Monthly-------");
+			int partTimeWages = (WorkingDayMonth * (FullDayHour / 2)) * perHour;
+			System.out.println("PartTime_Wages_for " + WorkingDayMonth + " days : " + partTimeWages + " Rs.");
+			break;
 		default:
 			System.out.println("Employee is Absent..!!!!");
 			break;
-					
+
 		}
-		in.close();		
+
 	}
 
 }
